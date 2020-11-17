c27526d21027c1f08b0526a44ec15a88c6a75533
Diana <Diana@DESKTOP-AR2QDPU>
2020-11-17 13:48:20 +0200

    lab7(cu greseli)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AutoLotModel;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,19 +21,456 @@ namespace Chisu_Diana_Lab6
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+
     public partial class MainWindow : Window
     {
+        ActionState action = ActionState.Nothing;
+        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
+        CollectionViewSource customerViewSource;
+        CollectionViewSource inventoryViewSource;
+        CollectionViewSource inventoryOrdersViewSource;
+
+        Binding txtFirstNameBinding = new Binding();
+        Binding txtLastNameBinding = new Binding();
+        Binding txtColorBinding = new Binding();
+        Binding txtMakeBinding = new Binding();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            txtFirstNameBinding.Path = new PropertyPath("FirstName");
+            txtLastNameBinding.Path = new PropertyPath("LastName");
+            lastNameTextBox.SetBinding(TextBox.TextProperty, txtLastNameBinding);
+            firstNameTextBox.SetBinding(TextBox.TextProperty, txtFirstNameBinding);
+            txtColorBinding.Path = new PropertyPath("Color");
+            txtMakeBinding.Path = new PropertyPath("Make");
+            colorTextBox.SetBinding(TextBox.TextProperty, txtColorBinding);
+            makeTextBox.SetBinding(TextBox.TextProperty, txtMakeBinding);
+
+
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // customerViewSource.Source = [generic data source]
+            customerViewSource =
+            ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            customerViewSource.Source = ctx.Customers.Local;
+            ctx.Customers.Load();
+
+            inventoryViewSource =
+            ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
+            inventoryViewSource.Source = ctx.Inventories.Local;
+            ctx.Inventories.Load();
+
+            inventoryOrdersViewSource =
+            ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryOrdersViewSource")));
+            //inventoryOrdersViewSource.Source = ctx.Orders.Local;
+            ctx.Orders.Load();
+            ctx.Inventories.Load();
+            cmbCustomers.ItemsSource = ctx.Customers.Local;
+            //cmbCustomers.DisplayMemberPath = "FirstName";
+            cmbCustomers.SelectedValuePath = "CustId";
+            cmbInventory.ItemsSource = ctx.Inventories.Local;
+            //cmbInventory.DisplayMemberPath = "Make";
+            cmbInventory.SelectedValuePath = "CarId";
+            BindDataGrid();
+
+
         }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            Customer customer = null;
+            Inventory inventory = null;
+
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    //instantiem Customer entity
+                    customer = new Customer()
+                    {
+                        FirstName = firstNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim()
+                    };
+                    inventory = new Inventory()
+                    {
+                        Color = colorTextBox.Text.Trim(),
+                        Make = makeTextBox.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Customers.Add(customer);
+                    customerViewSource.View.Refresh();
+                    ctx.Inventories.Add(inventory);
+                    inventoryViewSource.View.Refresh();
+
+                    ctx.SaveChanges();
+                }
+                //using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                customerDataGrid.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+
+            }
+            else
+                if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firstNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                    inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                    inventory.Color = colorTextBox.Text.Trim();
+                    inventory.Make = makeTextBox.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                inventoryViewSource.View.Refresh();
+                // pozitionarea pe item-ul curent
+                customerViewSource.View.MoveCurrentTo(customer);
+                inventoryViewSource.View.MoveCurrentTo(customer);
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                customerDataGrid.IsEnabled = true;
+                inventoryDataGrid.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+                firstNameTextBox.SetBinding(TextBox.TextProperty, txtFirstNameBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, txtLastNameBinding); customerDataGrid.IsEnabled = true;
+                colorTextBox.IsEnabled = false;
+                makeTextBox.IsEnabled = false;
+                colorTextBox.SetBinding(TextBox.TextProperty, txtColorBinding);
+                makeTextBox.SetBinding(TextBox.TextProperty, txtMakeBinding);
+
+
+
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customers.Remove(customer);
+                    inventory = (Inventory)inventoryDataGrid.SelectedItem;
+                    ctx.Inventories.Remove(inventory);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                inventoryViewSource.View.Refresh();
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                customerDataGrid.IsEnabled = true;
+                firstNameTextBox.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+                firstNameTextBox.SetBinding(TextBox.TextProperty, txtFirstNameBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, txtLastNameBinding);
+                inventoryDataGrid.IsEnabled = true;
+                colorTextBox.IsEnabled = false;
+                makeTextBox.IsEnabled = false;
+                colorTextBox.SetBinding(TextBox.TextProperty, txtColorBinding);
+                makeTextBox.SetBinding(TextBox.TextProperty, txtMakeBinding);
+
+            }
+            SetValidationBinding();
+        }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            firstNameTextBox.IsEnabled = true;
+            lastNameTextBox.IsEnabled = true;
+            customerDataGrid.IsEnabled = false;
+            inventoryDataGrid.IsEnabled = false;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            firstNameTextBox.Text = "";
+            lastNameTextBox.Text = "";
+            Keyboard.Focus(firstNameTextBox);
+            BindingOperations.ClearBinding(colorTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(makeTextBox, TextBox.TextProperty);
+            colorTextBox.Text = "";
+            makeTextBox.Text = "";
+            Keyboard.Focus(colorTextBox);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+            string tempFirstName = firstNameTextBox.Text.ToString();
+            string tempLastName = lastNameTextBox.Text.ToString();
+            string tempColor = colorTextBox.Text.ToString();
+            string tempMake = makeTextBox.Text.ToString();
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            customerDataGrid.IsEnabled = false;
+            inventoryDataGrid.IsEnabled = false;
+            firstNameTextBox.IsEnabled = true;
+            lastNameTextBox.IsEnabled = true;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            firstNameTextBox.Text = tempFirstName;
+            lastNameTextBox.Text = tempLastName;
+            Keyboard.Focus(firstNameTextBox);
+            colorTextBox.IsEnabled = true;
+            makeTextBox.IsEnabled = true;
+            BindingOperations.ClearBinding(colorTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(makeTextBox, TextBox.TextProperty);
+            colorTextBox.Text = tempColor;
+            makeTextBox.Text = tempMake;
+            Keyboard.Focus(colorTextBox);
+            SetValidationBinding();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+            string tempFirstName = firstNameTextBox.Text.ToString();
+            string tempLastName = lastNameTextBox.Text.ToString();
+            string tempColor = colorTextBox.Text.ToString();
+            string tempMake = makeTextBox.Text.ToString();
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            customerDataGrid.IsEnabled = false;
+            inventoryDataGrid.IsEnabled = false;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            firstNameTextBox.Text = tempFirstName;
+            lastNameTextBox.Text = tempLastName;
+            BindingOperations.ClearBinding(colorTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(makeTextBox, TextBox.TextProperty);
+            colorTextBox.Text = tempColor;
+            makeTextBox.Text = tempMake;
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Nothing;
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            btnPrevious.IsEnabled = true;
+            btnNext.IsEnabled = true;
+            customerDataGrid.IsEnabled = false;
+            inventoryDataGrid.IsEnabled = false;
+            firstNameTextBox.IsEnabled = false;
+            lastNameTextBox.IsEnabled = false;
+            firstNameTextBox.SetBinding(TextBox.TextProperty, txtFirstNameBinding);
+            lastNameTextBox.SetBinding(TextBox.TextProperty, txtLastNameBinding);
+            colorTextBox.IsEnabled = false;
+            makeTextBox.IsEnabled = false;
+            colorTextBox.SetBinding(TextBox.TextProperty, txtColorBinding);
+            makeTextBox.SetBinding(TextBox.TextProperty, txtMakeBinding);
+
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToNext();
+           
+        }
+        private void btnPrevious1_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext1_Click(object sender, RoutedEventArgs e)
+        {
+            inventoryViewSource.View.MoveCurrentToNext();
+        }
+
+        private void btnPreviousO_Click(object sender, RoutedEventArgs e)
+        {
+              inventoryOrdersViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNextO_Click(object sender, RoutedEventArgs e)
+        {
+        inventoryOrdersViewSource.View.MoveCurrentToNext();
+    }
+
+
+        private void btnSaveO_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    Customer customer = (Customer)cmbCustomers.SelectedItem;
+                    Inventory inventory = (Inventory)cmbInventory.SelectedItem;
+                    //instantiem Order entity
+                    order = new Order()
+                    {
+
+                        CustId = customer.CustId,
+                        CarId = inventory.CarId
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Orders.Add(order);
+                    inventoryOrdersViewSource.View.Refresh();
+                    //salvam modificarile
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+             if (action == ActionState.Edit)
+            {
+                dynamic selectedOrder = ordersDataGrid.SelectedItem;
+                try
+                {
+                    int curr_id = selectedOrder.OrderId;
+                    var editedOrder = ctx.Orders.FirstOrDefault(s => s.OrderId == curr_id);
+                    if (editedOrder != null)
+                    {
+                        editedOrder.CustId = Int32.Parse(cmbCustomers.SelectedValue.ToString());
+                    editedOrder.CarId = Convert.ToInt32(cmbInventory.SelectedValue.ToString());
+                        //salvam modificarile
+                        ctx.SaveChanges();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                BindDataGrid();
+                // pozitionarea pe item-ul curent
+                customerViewSource.View.MoveCurrentTo(selectedOrder);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    dynamic selectedOrder = ordersDataGrid.SelectedItem;
+                    int curr_id = selectedOrder.OrderId;
+                    var deletedOrder = ctx.Orders.FirstOrDefault(s => s.OrderId == curr_id);
+                    if (deletedOrder != null)
+                    {
+                        ctx.Orders.Remove(deletedOrder);
+                        ctx.SaveChanges();
+                        MessageBox.Show("Order Deleted Successfully", "Message");
+                        BindDataGrid();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void BindDataGrid()
+        {
+            var queryOrder = from ord in ctx.Orders
+                             join cust in ctx.Customers on ord.CustId equals
+                             cust.CustId
+                             join inv in ctx.Inventories on ord.CarId
+                 equals inv.CarId
+                             select new
+                             {
+                                 ord.OrderId,
+                                 ord.CarId,
+                                 ord.CustId,
+                                 cust.FirstName,
+                                 cust.LastName,
+                                 inv.Make,
+                                 inv.Color
+                             };
+            inventoryOrdersViewSource.Source = queryOrder.ToList();
+        }
+        private void SetValidationBinding()
+        {
+            Binding firstNameValidationBinding = new Binding();
+            firstNameValidationBinding.Source = customerViewSource;
+            firstNameValidationBinding.Path = new PropertyPath("FirstName");
+            firstNameValidationBinding.NotifyOnValidationError = true;
+            firstNameValidationBinding.Mode = BindingMode.TwoWay;
+            firstNameValidationBinding.UpdateSourceTrigger =
+           UpdateSourceTrigger.PropertyChanged;
+            //string required
+            firstNameValidationBinding.ValidationRules.Add(new StringNotEmpty());
+            firstNameTextBox.SetBinding(TextBox.TextProperty,
+           firstNameValidationBinding);
+            Binding lastNameValidationBinding = new Binding();
+            lastNameValidationBinding.Source = customerViewSource;
+            lastNameValidationBinding.Path = new PropertyPath("LastName");
+            lastNameValidationBinding.NotifyOnValidationError = true;
+            lastNameValidationBinding.Mode = BindingMode.TwoWay;
+            lastNameValidationBinding.UpdateSourceTrigger =
+           UpdateSourceTrigger.PropertyChanged;
+            //string min length validator
+            lastNameValidationBinding.ValidationRules.Add(new StringMinLengthValidator());
+            lastNameTextBox.SetBinding(TextBox.TextProperty,
+            lastNameValidationBinding); //setare binding nou
+        }
+
+        
     }
 }
---
+++ dbo.Order.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [OrderId] INT NOT NULL PRIMARY KEY IDENTITY,
+    [CustId]  INT NULL,
+    [CarId]   INT NULL,
+    PRIMARY KEY CLUSTERED ([OrderId] ASC),
+    CONSTRAINT [fk_orders_cust_id] FOREIGN KEY ([CustId]) REFERENCES [dbo].[Customer] ([CustId]) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT [fk_orders_inv_id] FOREIGN KEY ([CarId]) REFERENCES [dbo].[Inventory] ([CarId]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
